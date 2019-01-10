022c72b8f279389a591a82844361a34bfc456825
Ed <edward.berezitsky@gmail.com>
2019-01-10 11:55:29 -0500

    NIFI-5869 Support Reconnection for JMS
    resets worker if it doesn't work anymore for any reason. this will add "reconnect" capabilities. Will solve issues for following use cases:
    - authentication changed after successful connection
    - JNDI mapping changed and requires recaching.
    - JMS server isn't available anymore or restarted.
    
    improved controller reset on exception
    
    Signed-off-by: Matthew Burgess <mattyb149@apache.org>
    
    This closes #3261

--- nifi-nar-bundles/nifi-jms-bundle/nifi-jms-processors/src/main/java/org/apache/nifi/jms/processors/AbstractJMSProcessor.java
+++ nifi-nar-bundles/nifi-jms-bundle/nifi-jms-processors/src/main/java/org/apache/nifi/jms/processors/AbstractJMSProcessor.java
@@ -155,8 +155,31 @@ abstract class AbstractJMSProcessor<T extends JMSWorker> extends AbstractProcess
             worker = buildTargetResource(context);
         }
 
-        rendezvousWithJms(context, session, worker);
-        workerPool.offer(worker);
+        try {
+            rendezvousWithJms(context, session, worker);
+        } finally {
+            //in case of exception during worker's connection (consumer or publisher),
+            //an appropriate service is responsible to invalidate the worker.
+            //if worker is not valid anymore, don't put it back into a pool, try to rebuild it first, or discard.
+            //this will be helpful in a situation, when JNDI has changed, or JMS server is not available
+            //and reconnection is required.
+            if (worker == null || !worker.isValid()){
+                getLogger().debug("Worker is invalid. Will try re-create... ");
+                final JMSConnectionFactoryProviderDefinition cfProvider = context.getProperty(CF_SERVICE).asControllerService(JMSConnectionFactoryProviderDefinition.class);
+                try {
+                    // Safe to cast. Method #buildTargetResource(ProcessContext context) sets only CachingConnectionFactory
+                    CachingConnectionFactory currentCF = (CachingConnectionFactory)worker.jmsTemplate.getConnectionFactory();
+                    cfProvider.resetConnectionFactory(currentCF.getTargetConnectionFactory());
+                    worker = buildTargetResource(context);
+                }catch(Exception e) {
+                    getLogger().error("Failed to rebuild:  " + cfProvider);
+                    worker = null;
+                }
+            }
+            if (worker != null) {
+                workerPool.offer(worker);
+            }
+        }
     }
 
     @OnScheduled
