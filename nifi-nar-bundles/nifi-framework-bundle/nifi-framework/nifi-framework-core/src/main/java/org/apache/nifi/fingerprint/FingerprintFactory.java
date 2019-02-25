6f56a5f86322f01e99b87247c9aeadb750303b52
Bryan Bende <bbende@apache.org>
2019-02-25 16:52:41 -0500

    NIFI-6069 Fixing checkstyle error

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-core/src/main/java/org/apache/nifi/fingerprint/FingerprintFactory.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-core/src/main/java/org/apache/nifi/fingerprint/FingerprintFactory.java
@@ -760,7 +760,7 @@ public class FingerprintFactory {
             }
         };
     }
-    
+
     private Comparator<Element> getElementTextComparator() {
         return new Comparator<Element>() {
             @Override
