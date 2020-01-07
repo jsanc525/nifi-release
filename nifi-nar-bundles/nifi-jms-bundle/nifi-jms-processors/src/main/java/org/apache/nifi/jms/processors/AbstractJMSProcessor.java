0728837e443e5d89d6f0072d001c2e0c137f9812
Gardella Juan Pablo <juanc@pitzil.com>
2020-01-07 03:42:43 -0300

    NIFI-6915 This closes #3961. Jms Durable non shared subscription is broken Revert NIFI-4834 enhancement for durable non shared consumers only.
    Updated also AbstractJMSProcessor class to be public. The testing are not working
    without that change, as org.apache.nifi.jms.processors.PublishJMSIT and
    org.apache.nifi.jms.processors.ConsumeJMSIT are not working, as @OnSchedule
    method is not called (because it is not public).
    The method org.apache.nifi.util.StandardProcessorTestRunner.run(int iterations, boolean stopOnFinish, boolean initialize, long runWait) uses ReflectionUtils.invokeMethodsWithAnnotation which does not call non public
    methods.
    
    Signed-off-by: Joe Witt <joewitt@apache.org>

--- nifi-nar-bundles/nifi-jms-bundle/nifi-jms-processors/src/main/java/org/apache/nifi/jms/processors/AbstractJMSProcessor.java
+++ nifi-nar-bundles/nifi-jms-bundle/nifi-jms-processors/src/main/java/org/apache/nifi/jms/processors/AbstractJMSProcessor.java
@@ -29,6 +29,7 @@ import org.apache.nifi.processor.Processor;
 import org.apache.nifi.processor.exception.ProcessException;
 import org.apache.nifi.processor.util.StandardValidators;
 import org.springframework.jms.connection.CachingConnectionFactory;
+import org.springframework.jms.connection.SingleConnectionFactory;
 import org.springframework.jms.connection.UserCredentialsConnectionFactoryAdapter;
 import org.springframework.jms.core.JmsTemplate;
 
@@ -48,7 +49,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @see ConsumeJMS
  * @see JMSConnectionFactoryProviderDefinition
  */
-abstract class AbstractJMSProcessor<T extends JMSWorker> extends AbstractProcessor {
+public abstract class AbstractJMSProcessor<T extends JMSWorker> extends AbstractProcessor {
 
     static final String QUEUE = "QUEUE";
     static final String TOPIC = "TOPIC";
@@ -142,6 +143,10 @@ abstract class AbstractJMSProcessor<T extends JMSWorker> extends AbstractProcess
         propertyDescriptors.add(CHARSET);
     }
 
+    protected static String getClientId(ProcessContext context) {
+        return context.getProperty(CLIENT_ID).evaluateAttributeExpressions().getValue();
+      }
+
     @Override
     protected List<PropertyDescriptor> getSupportedPropertyDescriptors() {
         return propertyDescriptors;
@@ -230,12 +235,7 @@ abstract class AbstractJMSProcessor<T extends JMSWorker> extends AbstractProcess
         cfCredentialsAdapter.setPassword(context.getProperty(PASSWORD).getValue());
 
         final CachingConnectionFactory cachingFactory = new CachingConnectionFactory(cfCredentialsAdapter);
-
-        String clientId = context.getProperty(CLIENT_ID).evaluateAttributeExpressions().getValue();
-        if (clientId != null) {
-            clientId = clientId + "-" + clientIdCounter.getAndIncrement();
-            cachingFactory.setClientId(clientId);
-        }
+        setClientId(context, cachingFactory);
 
         JmsTemplate jmsTemplate = new JmsTemplate();
         jmsTemplate.setConnectionFactory(cachingFactory);
@@ -243,4 +243,21 @@ abstract class AbstractJMSProcessor<T extends JMSWorker> extends AbstractProcess
 
         return finishBuildingJmsWorker(cachingFactory, jmsTemplate, context);
     }
+
+    /**
+     * Set clientId for JMS connections when <tt>clientId</tt> is not null.
+     * It is overridden by {@code}ConsumeJMS{@code} when durable subscriptions
+     * is configured on the processor.
+     * @param context context.
+     * @param connectionFactory the connection factory.
+     * @since NIFI-6915
+     */
+    protected void setClientId(ProcessContext context, final SingleConnectionFactory connectionFactory) {
+        String clientId = getClientId(context);
+        if (clientId != null) {
+            clientId = clientId + "-" + clientIdCounter.getAndIncrement();
+            connectionFactory.setClientId(clientId);
+        }
+    }
+
 }
