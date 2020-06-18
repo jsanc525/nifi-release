cbfff0447263cc0c76f757f4cfdccd987e76d32a
Ferenc Kis <fkis@cloudera.com>
2020-06-18 15:50:44 +0200

    HOTFIX-3211 Fix merge issues

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-core/src/main/java/org/apache/nifi/controller/repository/WriteAheadFlowFileRepository.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-core/src/main/java/org/apache/nifi/controller/repository/WriteAheadFlowFileRepository.java
@@ -349,7 +349,7 @@ public class WriteAheadFlowFileRepository implements FlowFileRepository, SyncLis
         }
 
         // Once the content claim counts have been updated for all records, collect any transient claims that are eligible for destruction
-        for (final RepositoryRecord record : repositoryRecords) {
+        for (final RepositoryRecord record : records) {
             final List<ContentClaim> transientClaims = record.getTransientClaims();
             if (transientClaims != null) {
                 for (final ContentClaim transientClaim : transientClaims) {
